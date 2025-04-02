8bfe40ec140b2356e492c853400acc280561e3f0
JavierWWH <javierwongwh@gmail.com>
2025-04-02 21:10:33 +1000

    Add files via upload

---
+++ firmware/line-follower/include/Arduino_pin.h
@@ -0,0 +1,15 @@
+// Define sensor pins(from Right to Left) using #define
+#define s1 A3
+#define s2 A2
+#define s3 A1
+#define s4 A0
+#define s5 A8
+#define s6 A7
+#define s7 A11
+#define s8 A6
+
+// Motor Pins
+#define motorLeftDir 7      // Direction pin for left motor
+#define motorLeftPWM 3      // PWM pin for left motor speed
+#define motorRightDir 17     // Direction pin for right motor
+#define motorRightPWM 11    // PWM pin for right motor speed
