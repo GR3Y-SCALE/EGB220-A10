ae92d8e1f11c67697ba55d72ba6c5f56ac3b74cc
Maurice Blake <mauricertownsend@gmail.com>
2025-04-03 11:58:44 +1000

    Added main.cpp function structure prior to writing code

---
+++ firmware/payload/src/main.cpp
@@ -0,0 +1,14 @@
+#include<Arduino.h>
+
+void function (); //declare function here
+
+void setup () {
+    //setup
+}
+void loop () {
+    //loop
+}
+
+void function () {
+    // Define function here after declaration
+}
