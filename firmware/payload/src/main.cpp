f71f4cdf4ecfb8849cc97a0b693d8c4163b1f7f7
Maurice Blake <mauricertownsend@gmail.com>
2025-04-05 16:59:06 +1000

    Fixed logic of sensor detection

--- firmware/payload/src/main.cpp
+++ firmware/payload/src/main.cpp
@@ -5,11 +5,7 @@
 #define S2 PIN_F1
 #define S3 PIN_F4
 #define S4 PIN_F5
-const int8_t IRSENSORS[4] = {S3,S2,S4,S1}; // Order of IR sensors on the PCB
-const int8_t numSensors = 4;
 #define laser_en PIN_F6
-Servo servoX;
-Servo servoY;
 #define SZ_sig PIN_B7
 #define TRK_sig PIN_D0
 #define CAL_BTN PIN_D1
@@ -26,12 +22,18 @@ Servo servoY;
 
 */
 
+Servo servoX;
+Servo servoY;
+const int8_t IRSENSORS[4] = {S3,S2,S4,S1}; // Order of IR sensors on the PCB
+const int8_t numSensors = 4;
 double sensorCalibration[numSensors] = {0,0,0,0};
 const float detectionThreshold = 0.100; // 100mV drop from background level
+const float hysteresisMargin = 0.020; // 20mV hysteresis margin to stop fluctuating sensor states. 
 int8_t servoXRestPos = 90;
 int8_t servoYRestPos = 90;
 volatile bool calibrationRequested = false;
 
+// Function definitions
 void calibrateSensors();
 bool readSensor(int8_t i);
 bool targetAcquired();
@@ -56,27 +58,32 @@ void setup () {
     servoY.write(servoYRestPos);
 
     pinMode(CAL_BTN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(CAL_BTN), onCalibrateISR, FALLING);
+    // attachInterrupt(digitalPinToInterrupt(CAL_BTN), onCalibrateISR, FALLING);
 }
 void loop () {
-    if (calibrationRequested) {
-        calibrateSensors();
-        calibrationRequested = false;
+    // TODO:
+    // if (calibrationRequested) {
+    //     calibrateSensors();
+    //     calibrationRequested = false;
+    // }
+    if (!digitalRead(CAL_BTN)) {
+        calibrateSensors(); // Calibrate when CAL button has been set to low, it is on internal pull up.
     }
-    bool SZ = digitalRead(SZ_sig); // Read SlowZone Signal pin from Line Follower
+
     bool TRK = false;
     bool laser = false;
 
-    if (SZ) { // If in slow zone, activate turret
+    if (digitalRead(SZ_sig)) { // If in slow zone, activate turret. This will be a 3v3 signal from the line-follower mainboard.
         if (targetAcquired()) {
             TRK = true;
             int8_t dx = 0;
             int8_t dy = 0;
             for (int8_t i = 0; i < numSensors; i++) {
-                dx += readSensor(i) * sensor_dx[i];
-                dy += readSensor(i) * sensor_dy[i];
+                bool sensorValue = readSensor(i);
+                dx += sensorValue * sensor_dx[i];
+                dy += sensorValue * sensor_dy[i];
             }
-            int8_t norm_dx = (dx > 0) - (dx < 0);
+            int8_t norm_dx = (dx > 0) - (dx < 0); //Normalise the difference in dx dy to 1 or 0.
             int8_t norm_dy = (dy > 0) - (dy < 0);
             if (norm_dx || norm_dy) {
                 servoX.write(constrain(servoX.read() + norm_dx * 2, 0, 180));
@@ -93,50 +100,55 @@ void loop () {
         digitalWrite(TRK_sig,TRK);
         digitalWrite(laser_en,laser);
 
-        delay(20);
     } else {
         TRK = false;
         laser = false;
         servoX.write(servoXRestPos);
         servoY.write(servoYRestPos);
     }
+    delay(20);
 }
 
 void calibrateSensors() {
     for (int8_t i = 0; i < numSensors; i++) {
-        sensorCalibration[i] = analogRead(IRSENSORS[i]) * (5.0 / 1023.0); // converts to V
+        sensorCalibration[i] = analogRead(IRSENSORS[i]) * (5.0 / 1023.0); // converts to Volts
     }
 }
 
 bool readSensor(int8_t i) {
     double rawSensorValue = analogRead(IRSENSORS[i]) * (5.0 / 1023.0);
-    return (rawSensorValue - sensorCalibration[i] > detectionThreshold);
+    double delta = sensorCalibration[i] - rawSensorValue; // Sensor conduct when target is in view, dropping voltage.
+    
+    return delta > detectionThreshold; // Threshold to differentiate between background noise and target.
 }
 
-bool targetAcquired() {
-    bool tracking = false;
+bool targetAcquired() { // Check if any sensors see the target.
     for (int8_t i = 0; i < numSensors; i++) {
-        if (readSensor(i)) tracking = true;
+        if (readSensor(i)) return true;
     }
-    return tracking;
+    return false;
 }
 
 void scanServoX() {
     static int8_t angle = 0;
     static bool dir = 1;
 
-    angle = (servoX.read() + dir * 5);
-    if (angle >= 180 || angle <= 0) {
-        dir = !dir;
+    if (angle >= 180) {
+        angle = 180;
+        dir = false;
+    } else if (angle <= 0) {
+        angle = 0;
+        dir = true;
     }
     servoX.write(constrain(angle, 0, 180));
 }
 
-void onCalibrateISR() {
-    static unsigned long lastInterruptTime = 0;
-    unsigned long now = millis();
-    if (now - lastInterruptTime > 50) {
-        calibrationRequested = true;
-        lastInterruptTime = now;
-    }
-}
+// This is a nice to have, get working later.
+// void onCalibrateISR() {
+//     static unsigned long lastInterruptTime = 0;
+//     unsigned long now = millis();
+//     if (now - lastInterruptTime > 50) {
+//         calibrationRequested = true;
+//         lastInterruptTime = now;
+//     }
+// }
