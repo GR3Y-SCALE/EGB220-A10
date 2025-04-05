d98bd76026cbe674320c74d0a2119ca0840d7e43
Maurice Blake <mauricertownsend@gmail.com>
2025-04-05 18:00:48 +1000

    Removed floating point arithmetic

--- firmware/payload/src/main.cpp
+++ firmware/payload/src/main.cpp
@@ -5,9 +5,9 @@
 #define S2 PIN_A1
 #define S3 PIN_A2
 #define S4 PIN_A3
-#define laser_en PIN0
-#define SZ_sig 1
-#define TRK_sig 2
+#define LASER_EN PIN0
+#define SZ_SIG 1
+#define TRK_SIG 2
 #define CAL_BTN 8
 
 /* SENSOR CONFIGURATION
@@ -23,19 +23,20 @@
 */
 Servo servoX;
 Servo servoY;
-const int8_t IRSENSORS[4] = {S1,S2,S3,S4}; // Order of IR sensors on the PCB
+const int8_t IRSENSORS[4] = {S2,S3,S1,S4}; // Order of IR sensors on the PCB
 const int8_t numSensors = 4;
-double sensorCalibration[numSensors] = {0,0,0,0};
-const float detectionThreshold = 0.150; // 100mV drop from background level
+
+int sensorCalibration[numSensors] = {0,0,0,0};
+const int detectionThreshold = 20; // 20 5mV increments, therefore 100mV threshold
+
 int8_t servoXRestPos = 90;
 int8_t servoYRestPos = 90;
-volatile bool calibrationRequested = false;
 
+// Function definitions
 void calibrateSensors();
 bool readSensor(int8_t i);
 bool targetAcquired();
 void scanServoX();
-void onCalibrateISR();
 
 const int8_t sensor_dx[numSensors] = {-1,1,-1,1};
 const int8_t sensor_dy[numSensors] = {1,1,-1,-1};
@@ -45,47 +46,36 @@ void setup () {
     for (int8_t i = 0; i < numSensors; i++) {
         pinMode(IRSENSORS[i],INPUT);
     }
-    pinMode(laser_en, OUTPUT);
-    pinMode(SZ_sig, INPUT);
+    pinMode(LASER_EN, OUTPUT);
+    pinMode(SZ_SIG, INPUT);
     servoX.attach(9);
     servoY.attach(10);
-    pinMode(TRK_sig, OUTPUT);
-    digitalWrite(TRK_sig,LOW);
+    pinMode(TRK_SIG, OUTPUT);
+    digitalWrite(TRK_SIG,LOW);
     servoX.write(servoXRestPos);
     servoY.write(servoYRestPos);
 
-    pinMode(CAL_BTN, INPUT);
-    // attachInterrupt(digitalPinToInterrupt(CAL_BTN), onCalibrateISR, FALLING);
-    Serial.begin(9600);
+    pinMode(CAL_BTN, INPUT_PULLUP);
 }
 void loop () {
-    for (int i = 0; i < 4; i++) {
-        Serial.print(sensorCalibration[i]);
-        Serial.print(" "); 
-      }
-      Serial.println();
+    if (!digitalRead(CAL_BTN)) {
+        calibrateSensors(); // Calibrate when CAL button has been set to low, it is on internal pull up.
+    }
 
-    if (CAL_BTN) {
-        calibrateSensors();
-    }
-    if (calibrationRequested) {
-        calibrateSensors();
-        calibrationRequested = false;
-    }
-    bool SZ = digitalRead(SZ_sig); // Read SlowZone Signal pin from Line Follower
     bool TRK = false;
     bool laser = false;
 
-    if (SZ) { // If in slow zone, activate turret
+    if (digitalRead(SZ_SIG)) { // If in slow zone, activate turret. This will be a 3v3 signal from the line-follower mainboard.
         if (targetAcquired()) {
             TRK = true;
             int8_t dx = 0;
             int8_t dy = 0;
             for (int8_t i = 0; i < numSensors; i++) {
-                dx += readSensor(i) * sensor_dx[i];
-                dy += readSensor(i) * sensor_dy[i];
+                bool sensorValue = readSensor(i);
+                dx += sensorValue * sensor_dx[i];
+                dy += sensorValue * sensor_dy[i];
             }
-            int8_t norm_dx = (dx > 0) - (dx < 0);
+            int8_t norm_dx = (dx > 0) - (dx < 0); //Normalise the difference in dx dy to 1 or 0.
             int8_t norm_dy = (dy > 0) - (dy < 0);
             if (norm_dx || norm_dy) {
                 servoX.write(constrain(servoX.read() + norm_dx * 2, 0, 180));
@@ -99,53 +89,48 @@ void loop () {
             scanServoX();
         }
 
-        digitalWrite(TRK_sig,TRK);
-        digitalWrite(laser_en,laser);
+        digitalWrite(TRK_SIG,TRK);
+        digitalWrite(LASER_EN,laser);
 
-        delay(20);
     } else {
         TRK = false;
         laser = false;
         servoX.write(servoXRestPos);
         servoY.write(servoYRestPos);
     }
+    delay(20);
 }
 
 void calibrateSensors() {
     for (int8_t i = 0; i < numSensors; i++) {
-        sensorCalibration[i] = analogRead(IRSENSORS[i] * (5.0 / 1023.0)); // converts to V
+        sensorCalibration[i] = analogRead(IRSENSORS[i]); // converts to Volts
     }
 }
 
 bool readSensor(int8_t i) {
-    double rawSensorValue = analogRead(IRSENSORS[i]) * (5.0 / 1023.0);
-    return (rawSensorValue - sensorCalibration[i] > detectionThreshold);
+    int rawSensorValue = analogRead(IRSENSORS[i]);
+    int delta = round(sensorCalibration[i] - rawSensorValue); // Sensor conduct when target is in view, dropping voltage.
+    
+    return delta > detectionThreshold; // Threshold to differentiate between background noise and target.
 }
 
-bool targetAcquired() {
-    bool tracking = false;
+bool targetAcquired() { // Check if any sensors see the target.
     for (int8_t i = 0; i < numSensors; i++) {
-        if (readSensor(i)) tracking = true;
+        if (readSensor(i)) return true;
     }
-    return tracking;
+    return false;
 }
 
 void scanServoX() {
     static int8_t angle = 0;
     static bool dir = 1;
-
-    angle = (servoX.read() + dir * 5);
-    if (angle >= 180 || angle <= 0) {
-        dir = !dir;
+    angle = (servoX.read() + (dir ? 5 : -5));
+    if (angle >= 180) {
+        angle = 180;
+        dir = false;
+    } else if (angle <= 0) {
+        angle = 0;
+        dir = true;
     }
     servoX.write(constrain(angle, 0, 180));
-}
-
-void onCalibrateISR() {
-    static unsigned long lastInterruptTime = 0;
-    unsigned long now = millis();
-    if (now - lastInterruptTime > 50) {
-        calibrationRequested = true;
-        lastInterruptTime = now;
-    }
 }
